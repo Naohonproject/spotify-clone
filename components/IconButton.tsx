5b246b3a05e524bb63fa2b005d60762aac08a016
LE TUAN BAO <LETUANBAO@mbg8.onmicrosoft.com>
2022-09-22 23:56:50 +0700

    connect spotify api

---
+++ components/IconButton.tsx
@@ -0,0 +1,16 @@
+// create interface for IconButton component props
+interface Props {
+  icon: (props: React.ComponentProps<"svg">) => JSX.Element;
+  label: String;
+}
+
+const IconButton = ({ icon: Icon, label }: Props) => {
+  return (
+    <button className="flex items-center space-x-2 hover:text-white">
+      <Icon className="icon" />
+      <span>{label}</span>
+    </button>
+  );
+};
+
+export default IconButton;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,67 @@
-import { HomeIcon } from "@heroicons/react/outline";
+import IconButton from "./IconButton";
+import {
+  HomeIcon,
+  HeartIcon,
+  SearchIcon,
+  LibraryIcon,
+  RssIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/outline";
+
+const Divider = () => <hr className="border-t-[0.1px] border-gray-800" />;
 const SideBar = () => {
   return (
-    <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen overflow-y-scroll sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block ">
+    <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen overflow-y-scroll scrollbar-hidden sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block ">
       <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="h-5 w-5" />
-          <span>Home</span>
-        </button>
+        <IconButton icon={HomeIcon} label="Home" />
+        <IconButton icon={SearchIcon} label="Search" />
+        <IconButton icon={LibraryIcon} label="Your Library" />
+        <Divider />
+
+        <IconButton icon={PlusCircleIcon} label="Create Library" />
+        <IconButton icon={HeartIcon} label="Like Song" />
+        <IconButton icon={RssIcon} label="Your Episode" />
+        <Divider />
+
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
+        <p className="cursor-pointer hover:text-white">Playlist</p>
       </div>
     </div>
   );
