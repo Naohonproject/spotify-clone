d51754b59a1f44383d0f5d65bd0aa50cc13e16b0
LE TUAN BAO <LETUANBAO@mbg8.onmicrosoft.com>
2022-09-24 16:13:16 +0700

    create sign-out button on center component

--- components/Center.tsx
+++ components/Center.tsx
@@ -1,5 +1,35 @@
+import { signOut, useSession } from "next-auth/react";
+import { usePlaylistContext } from "../contexts/PlayListContext";
+import Image from "next/image";
+import UserIcon from "../assets/avatar.jpg";
+import { ChevronDownIcon } from "@heroicons/react/outline";
+
 const Center = () => {
-  return <div>Center</div>;
+  const { playlistContextState } = usePlaylistContext();
+
+  const { data: session } = useSession();
+  return (
+    <div className="text-white flex-grow relative h-screen overflow-y-scroll scrollbar-hidden">
+      <header className="absolute top-5 right-8">
+        <div
+          onClick={() => {
+            signOut();
+          }}
+          className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full py-1 pl-1 pr-2"
+        >
+          <Image
+            src={session?.user?.image || UserIcon}
+            alt="User Avatar"
+            height="40px"
+            width="40px"
+            className="rounded-full object-cover"
+          />
+          <h2>{session?.user?.name}</h2>
+          <ChevronDownIcon className="icon" />
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Center;
